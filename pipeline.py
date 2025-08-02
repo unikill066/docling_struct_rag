cdc3f7f6f280ab831ec4f6ee7ae07654987864f6
Nikhil Inturi <discovery@Mac.lan>
2025-08-01 23:41:57 -0500

    index paps

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,17 @@
 from ingestion_pipe import extract_documents, chunk_documents, prepare_chunks, embed_and_store
 
+import os
+print(os.listdir("/Users/discovery/Desktop/Docling/data"))
+
 if __name__ == "__main__":
     sources = [
-        "https://arxiv.org/pdf/2408.09869",
-        "https://arxiv.org/pdf/2408.09869",
-        ""
-
+        "data/epigenomic_landscape_of_the_human_dorsal_root.16.pdf",
+        "data/persistent_changes_in_the_dorsal_root_ganglion.18.pdf",
+        "data/180190.1-20250520113907-covered-e0fd13ba177f913fd3156f593ead4cfd.pdf",
+        "data/NIHPP-2024.06.15.599167v1.pdf",
+        "data/2024.12.20.629638v1.full.pdf",
+        "data/2025.03.24.645122v1.full.pdf",
+        "data/2025.03.26.645611v1.full.pdf",
     ]
     documents = extract_documents(sources)
     chunks = chunk_documents(documents)
